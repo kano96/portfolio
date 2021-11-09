4d02b0984d1f850ae2f017396fdc68be9a103843
Kevin Torres <kevin_akd@hotmail.com>
2021-11-09 14:45:06 -0500

    dummy img

--- src/components/Mainview.jsx
+++ src/components/Mainview.jsx
@@ -14,63 +14,70 @@ function Mainview() {
   }, [type]);
   return (
     <section className="main-container">
-      <div className="contact-icons">
-        <ul className="icons-list">
-          <li className="linkedin">
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/kevin-torres-iglesias-520311167/"
-              rel="noreferrer"
-              className="contact-links"
-            >
-              <i class="fab fa-linkedin-in"></i>
-            </a>
-          </li>
-          <li className="github">
-            <a
-              target="_blank"
-              href="https://github.com/kano96"
-              rel="noreferrer"
-              className="contact-links"
-            >
-              <i class="fab fa-github"></i>
-            </a>
-          </li>
-          <li className="twitter">
-            <a
-              target="_blank"
-              href="https://twitter.com/kevinSDQ"
-              rel="noreferrer"
-              className="contact-links"
-            >
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li className="mail">
-            <a
-              target="_blank"
-              href="#contact"
-              rel="noreferrer"
-              className="contact-links"
-            >
-              <i class="fas fa-envelope"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="title-container">
-        <h1 className="blinking-cursor">{type}</h1>
-      </div>
-      <div className="descr">
-        <p>
-          Hi there, I'm Kevin, a full stack web developer with experience in
-          ReactJS and NodeJs
-        </p>
-      </div>
-      <div className="action-btn">
-        <button className="port-btn">
-          <a href="#portfolio">My Portfolio</a>
-        </button>
+      <div className="banner-container">
+        <div className="info">
+          <div className="contact-icons">
+            <ul className="icons-list">
+              <li className="linkedin">
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/kevin-torres-iglesias-520311167/"
+                  rel="noreferrer"
+                  className="contact-links"
+                >
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+              <li className="github">
+                <a
+                  target="_blank"
+                  href="https://github.com/kano96"
+                  rel="noreferrer"
+                  className="contact-links"
+                >
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+              <li className="twitter">
+                <a
+                  target="_blank"
+                  href="https://twitter.com/kevinSDQ"
+                  rel="noreferrer"
+                  className="contact-links"
+                >
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li className="mail">
+                <a
+                  target="_blank"
+                  href="#contact"
+                  rel="noreferrer"
+                  className="contact-links"
+                >
+                  <i class="fas fa-envelope"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="title-container">
+            <h1 className="blinking-cursor">{type}</h1>
+          </div>
+          <div className="descr">
+            <p>
+              Hi there, I'm Kevin, a full stack web developer with experience in
+              ReactJS and NodeJs
+            </p>
+          </div>
+          <div className="action-btn">
+            <button className="port-btn">
+              <a href="#portfolio">My Portfolio</a>
+            </button>
+          </div>
+        </div>
+        <div className="profile-img">
+          <img src="../assets/profile-img.png" alt="profile-kev" />
+        </div>
       </div>
     </section>
   );
