24cb1eb3cc674be87159336dde430b980d2800f2
Kevin Torres <kevin_akd@hotmail.com>
2021-11-09 00:10:48 -0500

    Resuelta barra de navigación mobile

---
+++ src/components/Nav/Menuitems.js
@@ -0,0 +1,8 @@
+const menuItems = [
+  { name: "Home", url: "#", cName: "nav-link" },
+  { name: "Skills", url: "#", cName: "nav-link" },
+  { name: "Portfolio", url: "#", cName: "nav-link" },
+  { name: "Contact", url: "#", cName: "nav-link" },
+];
+
+export default menuItems;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./Nav.css";
+import menuItems from "./Menuitems";
+import logo from "../../assets/LogoKevin.png";
+
+function Nav() {
+  const [actMenu, setactMenu] = useState(false);
+  const handleOnClick = (e) => {
+    setactMenu(!actMenu);
+  };
+  return (
+    <div className="allnav">
+      <div className="Nav-container">
+        <nav className="nav">
+          <div className="logo">
+            <img src={logo} alt="LogoKevin" width="50px" />
+          </div>
+          <div className="menu-icon" onClick={handleOnClick}>
+            <i className={actMenu ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </nav>
+      </div>
+      <div className={actMenu ? "menu-mobile-container" : "menu"}>
+        <div className="ul-container">
+          <ul className="menu-active-mobile">
+            {menuItems.map((link, index) => {
+              return (
+                <li key={index}>
+                  <a href={link.url} className={link.cName}>
+                    {link.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
