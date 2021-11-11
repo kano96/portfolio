6fb74af6e662f465b9fd337c2bae89350c6e6d28
Kevin Torres <kevin_akd@hotmail.com>
2021-11-11 09:53:29 -0500

    Resueltos completamente nav, main y skills

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 import menuItems from "./Menuitems";
 import logo from "../../assets/LogoKevin.png";
 
 function Nav() {
+  // Actuación del botón de menú para mobile
+
   const [actMenu, setactMenu] = useState(false);
   const handleOnClick = (e) => {
     setactMenu(!actMenu);
   };
 
+  // Actuación de la barra de navegación en scroll
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 400) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Efecto para escuchar el scroll
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
-    <div className="allnav" id="top">
+    <div className={isVisible ? "Nav-container-active" : "allnav"} id="top">
       <div className="Nav-container">
         <nav className="nav">
           <div className="logo">
