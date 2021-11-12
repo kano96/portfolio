832e1a5ad8b8e3c244a63043613748baa72c01c6
Kevin Torres <kevin_akd@hotmail.com>
2021-11-12 13:12:22 -0500

    a{ñadido contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,8 +6,38 @@ function Contact() {
     <div className="contact-container" id="contact">
       <div className="content">
         <div className="title">
-          <h1 style={{ fontSize: "35px" }}>Contact Me</h1>
+          <h1 style={{ fontSize: "35px" }}>Send me a message</h1>
+          <p>
+            Got a question or proposal, or just want to say hello? Go ahead.
+          </p>
         </div>
+        <form
+          action="https://formsubmit.co/kevintorres996@hotmail.com"
+          method="POST"
+        >
+          <div className="formulario">
+            <div className="row50">
+              <input type="text" placeholder="Your Name" required name="name" />
+            </div>
+            <div className="row50">
+              <input type="email" placeholder="Your Email" name="email" />
+            </div>
+            <div className="row100">
+              <input type="text" placeholder="Your Subject" />
+            </div>
+            <div className="row100">
+              <textarea
+                name=""
+                id="message"
+                required
+                placeholder="Your message"
+              ></textarea>
+            </div>
+            <div className="button">
+              <button type="submit">Send Message</button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
