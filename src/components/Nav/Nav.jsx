7f6a8293a98f48232ca5a96b684119694be8c7a4
Kevin Torres <kevin_akd@hotmail.com>
2021-11-18 16:54:46 -0500

    added scroll change to navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,25 +1,54 @@
 import React, { useState, useEffect } from "react";
 import "./Nav.css";
-
+import logo from "../../assets/LogoKevin.png";
 function Nav() {
   // Actuación del botón de menú para mobile
 
   const [actMenu, setActMenu] = useState(false);
+  const [navBar, setNavBar] = useState(false);
   const [screenW, setScreenW] = useState(window.innerWidth);
   const handleOnClick = (e) => {
     setActMenu(!actMenu);
   };
-  useEffect(() => {});
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenW(window.innerWidth);
+    };
+    const changeB = () => {
+      if (window.scrollY > 99) {
+        setNavBar(true);
+      } else {
+        setNavBar(false);
+      }
+    };
+    window.addEventListener("resize", changeWidth);
+    window.addEventListener("scroll", changeB);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
 
   return (
-    <nav>
-      {(actMenu || screenW >= 700) && (
+    <nav className={navBar ? "navbar active" : "navbar"}>
+      {(actMenu || screenW > 700) && (
         <ul className="list">
-          <li className="items">Home</li>
-          <li className="items">About</li>
-          <li className="items">Skills</li>
-          <li className="items">Porfolio</li>
-          <li className="items">Contact</li>
+          <li className="items">
+            <a href="#top">Home</a>
+          </li>
+          <li className="items">
+            <a href="#about">About</a>
+          </li>
+          <li className="items">
+            <a href="#skills">Skills</a>
+          </li>
+          <li className="items">
+            <a href="#portfolio">Porfolio</a>
+          </li>
+          <li className="items">
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       )}
       <button className="btn" onClick={handleOnClick}>
